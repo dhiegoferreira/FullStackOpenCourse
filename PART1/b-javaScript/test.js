e4a8cce102949e2500c0d3cf8b4d103089ee1977
dhiegoferreira <dev.dhiegoferreira@gmail.com>
2024-06-01 12:24:01 +0100

    feat: part1 continue

---
+++ PART1/b-javaScript/test.js
@@ -0,0 +1,18 @@
+// const t = [1, 2, 3, 4, 5]
+
+// const [first, second, ...rest] = t
+
+// console.log(first, second)  // 1, 2 is printed
+// console.log(rest)          // [3, 4, 5] is printedls
+
+
+const t = [1,,2,3]
+const tSquared = t.map(p => p * p)
+
+const sum = (p1, p2) => {
+    console.log(p1)
+    console.log(p2)
+    return p1 + p2
+  }
+
+
