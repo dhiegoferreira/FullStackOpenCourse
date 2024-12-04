44b91be784c1d6d81e064f75ae78912d46960047
dhiegoferreira <dev.dhiegoferreira@gmail.com>
2024-12-04 23:55:08 +0000

    P3-Ex.3.6

--- part3/phoneBookBackEnd/index.js
+++ part3/phoneBookBackEnd/index.js
@@ -82,6 +82,13 @@ app.post("/api/persons/", (request, response) => {
         })
     }
 
+    if(persons.find(person => person.name = body.name)){
+        return  response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+
 
     const person = {
         id: getRandom(),
