8ccd20c92055b185165f135251126b9214fa7566
dhiegoferreira <dev.dhiegoferreira@gmail.com>
2025-02-26 23:08:27 +0000

    [p3-b] Improvemente with notification and refactoring

--- part3/b-deploying-app-to-internet/notes-frontend/src/App.jsx
+++ part3/b-deploying-app-to-internet/notes-frontend/src/App.jsx
@@ -1,61 +1,75 @@
 import { useState, useEffect} from 'react'
 import axios from 'axios'
-import Note from './components/Note'
+import Note from './components/Note.jsx'
 import noteService from  './services/notes' 
+import Notification from './components/Notification.jsx'
 
 
-const baseUrl = 'http://localhost:3001/api/notes'
 
 const App = () => {
   
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(false)
-  
+  const [errorMessage, setErrorMessage] = useState(null)
+
+
   useEffect(() => {
     console.log('effect')
-    axios.get(baseUrl).then((response) => {
-      setNotes(response.data)
+    noteService.getAll().then((response) => {
+      setNotes(response)
     })
   }, [])
 
-  const toggleImportanceOf = (id) => {
-    
-    console.log(`importance of ${id} needs to be toggled.`)
-    const url = `${baseUrl}${id}`
-    const note = notes.find(n => n.id === id)
-    const changedNote = {...note, important: !note.important}
-  
-    noteService.update(id,changedNote).then(returnedNote => {
-      setNotes(notes.map(note => note.id !== id ? note : returnedNote))
-    })
-    .catch(error => {
-      alert(`the note "${note.content}" was already deleted form server.`)
-      //call back
-      setNotes(notes.filter(n => n.id !== id))
-    })
 
-
-  }
-  
   const addNote = (event) => {
     event.preventDefault()
+
     const noteObject = {
       content: newNote,
       important: Math.random() > 0.5,
       id: notes.length + 1,
     }
+
+    
+    noteService.create(noteObject)
+    .then(response => setNotes(notes.concat(response)))
   
-    axios
-    .post(baseUrl,noteObject)
-    .then(response => {
-      setNotes(notes.concat(noteObject))
-      setNewNote('')
-    })
-   
+    
+    setNewNote('')
     
   }
   
+
+
+
+  const toggleImportanceOf = (id) => {
+    
+    console.log(`importance of ${id} needs to be toggled.`)
+
+  
+    //const note = notes.find(n => n.id === id)
+    // const changedNote = {...note, important: !note.important}
+    
+ 
+
+    noteService.update(id).then((returnedNote) => {
+      setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+    })
+    .catch(error => {
+      setErrorMessage(
+        `Note '${note.content}' was already removed from server`
+      )
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+      //call back
+      setNotes(notes.filter(n => n.id !== id))
+    })
+
+  }
+  
+
   const handleNoteChange = (event) => {
     setNewNote(event.target.value)
   }
@@ -65,17 +79,22 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
         </button>
       </div>      
       <ul>
-        {notesToShow.map(note => 
-          <Note key={note.id} 
-                note={note}
-                toggleImportance={()=> toggleImportanceOf(note.id)} />
-        )}
+        <ul>
+          {notesToShow.map(note => 
+            <Note
+              key={note.id}
+              note={note}
+              toggleImportance={() => toggleImportanceOf(note.id)}
+            />
+          )}
+        </ul>
       </ul>
       <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange}/>
