f4241ac2830e4a6ce0103de1d40e2297901b2242
dhiegoferreira <dev.dhiegoferreira@gmail.com>
2025-03-03 22:28:04 +0000

    [p3-c] Ex3.13 and Ex3.14 fetching of all phonebook entries from database

--- part3/c-saving-data-to-mongoDB/phonebook-backend/index.js
+++ part3/c-saving-data-to-mongoDB/phonebook-backend/index.js
@@ -1,12 +1,15 @@
-
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
 var morgan = require('morgan')
 
+const Person = require('./models/person')
+
 
 app.use(cors())
 app.use(express.static('dist'))
+app.use(express.json())
 
 
 
@@ -14,35 +17,12 @@ app.use(express.static('dist'))
 
 // Define a custom token for the request body
 morgan.token('body', (req) => JSON.stringify(req.body));
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :body')
 )
 
-app.use(express.json()) 
 
-let persons = 
-    [
-        { 
-          "id": "1",
-          "name": "Arto Hellas", 
-          "number": "040-123456"
-        },
-        { 
-          "id": "2",
-          "name": "Ada Lovelace", 
-          "number": "39-44-5323523"
-        },
-        { 
-          "id": "3",
-          "name": "Dan Abramov", 
-          "number": "12-43-234345"
-        },
-        { 
-          "id": "4",
-          "name": "Mary Poppendieck", 
-          "number": "39-23-6423122"
-        }
-    ]
+
+
 
 
 app.get("/info", (request, response) => {
@@ -54,27 +34,42 @@ app.get("/info", (request, response) => {
     response.end(`Phonebook has info for ${amount} people\n\n${dateNow.toString()}`)
 })
 
-app.get("/api/persons", (request,response) => {
-    response.json(persons)
+
+
+app.get("/api/persons", (request, response) => {
+
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
+
 })
 
 
 app.get("/api/persons/:id", (request, response) => {
     const id = request.params.id
 
-    const person = persons.find(person => person.id == id) 
 
-    return person ? response.json(result) : response.status(402).end()
+    Person.find({}).then(persons => {
+        response.json(persons.id == id)
+    })
+
+    // return person ? response.json(result) : response.status(402).end()
 })
 
 
 app.delete("/api/persons/:id", (request, response) => {
-    
+
     const id = request.params.id
 
     const personToRemove = persons.filter(person => person.id !== id)
 
-    return personToRemove ? response.json(personToRemove) : response.status(402).end()
+    Person.find({}).then(
+        response.json(persons.filter(person => person.id !== id))
+    )
+
+
+
+    // return personToRemove ? response.json(personToRemove) : response.status(402).end()
 })
 
 
@@ -82,52 +77,56 @@ app.delete("/api/persons/:id", (request, response) => {
 
 
 app.post("/api/persons", (request, response) => {
-    
+
     const body = request.body;
 
-
-    if(!body.name){
-        return  response.status(400).json({
+    // console.log(body.name)
+    // console.log(body.number)
+    if (!body.name) {
+        return response.status(400).json({
             error: 'name missing'
-        });
+        }).end();
     }
 
-    if(!body.number){
-        return  response.status(400).json({
+    // console.log("after first validation")
+    if (!body.number) {
+        return response.status(400).json({
             error: 'number missing'
-        });
+        }).end();
     }
+    // console.log("after second validation")
 
-    if(persons.find(person => person.name === body.name)){
-        return  response.status(400).json({
-            error: 'name must be unique'
-        });
-    }
+    Person.find({ name: body.name }).then(result => {
+        response.status(400).end()
 
-    const person = {
-        id: getRandom(),
+    });
+
+
+    const person = new Person({
         name: body.name,
-        number: body.number 
-    };
+        number: body.number,
+    })
 
-    persons = persons.concat(person);
-    
-    response.json(persons);
-}) 
-
-function getRandom() {
-    return Math.random() * (0 - Number.MAX_SAFE_INTEGER) + Number.MAX_SAFE_INTEGER;
-  }
-
-const generateId = () => {
-    
-    const maxID = notes.length > 0 ? Math.max(...notes.map(n => Number(n.id))) : 0
-
-    return String(maxId + 1)
-}
+    person.save().then(savedPerson => {
+        response.json(savedPerson).end()
+    })
 
 
-const PORT = 3001
+})
+
+// function getRandom() {
+//     return Math.random() * (0 - Number.MAX_SAFE_INTEGER) + Number.MAX_SAFE_INTEGER;
+// }
+
+// const generateId = () => {
+
+//     const maxID = notes.length > 0 ? Math.max(...notes.map(n => Number(n.id))) : 0
+
+//     return String(maxId + 1)
+// }
+
+
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
