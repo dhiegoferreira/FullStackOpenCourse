2ca1130da6830c9a2885f0b22b58d0bded39a8b1
dhiegoferreira <dev.dhiegoferreira@gmail.com>
2025-03-04 16:53:34 +0000

    [p3-c] Ex3.17-3.18 adding error handling middleware and implement put request.

--- part3/c-saving-data-to-mongoDB/phonebook-backend/index.js
+++ part3/c-saving-data-to-mongoDB/phonebook-backend/index.js
@@ -2,8 +2,6 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-var morgan = require('morgan')
-
 const Person = require('./models/person')
 
 
@@ -11,16 +9,11 @@ app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 
-/* 3
-3.15 - deleting phonebook need works
-3.16 - error handling to error handler middleware
-3.17 - when create a new phonebook enty for peerson, update the phone nhumber for existing phonebook name
-3.18 - api/persons/:id and info routes to use the database, and verify that they work directly with the browser, Postman, or VS Code REST client.
-*/
 
-
-
+// Morgan set up
 // Define a custom token for the request body
+var morgan = require('morgan')
+
 morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :body')
 )
@@ -29,85 +22,101 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms - 
 
 
 
+app.get("/info", (request, response,next) => {
 
-app.get("/info", (request, response) => {
-
-    const amount = persons.length
 
     const dateNow = new Date(Date.now())
+    
+    Person.find({}).then(persons => 
+        response.end(`Phonebook has info for ${persons.length} people\n\n${dateNow.toString()}`)
+    )
+        .catch(error => next(error))
 
-    response.end(`Phonebook has info for ${amount} people\n\n${dateNow.toString()}`)
+
+    // console.log(amount)
+
+    
 })
 
 
 
-app.get("/api/persons", (request, response) => {
+app.get("/api/persons", (request, response, next) => {
 
     Person.find({}).then(persons => {
         response.json(persons)
     })
-        .catch(error => {
-            console.log(error)
-            response.status(400).end()
-        })
+        .catch(error => next(error))
 
 })
 
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response,next) => {
     const id = request.params.id
 
 
     Person.findById(id).then(persons => {
         response.json(persons.id == id).status(200).end()
     })
-        .catch(error => {
-            console.log(error)
-            response.status(400).end()
-        })
+        .catch(error => next(error))
 
 
 })
 
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
 
     Person.findByIdAndDelete(request.params.id)
         .then(result => {
             response.status(204).end()
+        }).catch(error => next(error))
+
+})
+
+
+app.put("/api/persons/:id", (request, response) => {
+
+    body = request.body
+
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+
+    Person.findByIdAndUpdate(request.params.id, person, {
+        new: true
+    })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
         })
         .catch(error => next(error))
 
 })
 
 
-
-
-
 app.post("/api/persons", (request, response) => {
 
     const body = request.body;
 
-  
+
     if (!body.name) {
         return response.status(400).json({
             error: 'name missing'
         }).end();
     }
 
-   
+
     if (!body.number) {
         return response.status(400).json({
             error: 'number missing'
         }).end();
     }
-   
+
     Person.find({ name: body.name }).then(result => {
         response.status(400).end()
 
-    }).catch(error => {
-        response.status(400).end()
-    });
+    }).catch(error => next(error))
 
 
     const person = new Person({
@@ -117,15 +126,34 @@ app.post("/api/persons", (request, response) => {
 
     person.save().then(savedPerson => {
         response.json(savedPerson).end()
-    })
-    .catch(error => {
-        console.log(error)
-        response.status(400).end()
-    })
-
+    }).catch(error => next(error))
 
 
 })
+
+
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
+
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
 
 
 const PORT = process.env.PORT;
