a083f330dc76c56767ad287a8fb06ad6362acd22
dhiegoferreira <dev.dhiegoferreira@gmail.com>
2025-03-12 20:54:29 +0000

    [p4-a] Ex.4.5

--- part4/a-strucutre-backend-testing/blog-list-backend/tests/list_helper.test.js
+++ part4/a-strucutre-backend-testing/blog-list-backend/tests/list_helper.test.js
@@ -22,6 +22,45 @@ describe('total likes', () => {
         assert.strictEqual(listHelper.totalLikes(listWithOneBlog),5)
     })
 
+})
 
+
+describe('the most likes on blogs', () => {
+
+
+    const blogs = [
+    {
+        _id :'5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsgar W. Dijkstra',
+        url:'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 5,
+        __v: 0
+    },
+    {
+        _id :'6a422aa71b54a676234d17f8',
+        title: 'Second TITLE',
+        author: 'Edsgar W. Dijkstra',
+        url:'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 10,
+        __v: 0
+    }
+    ,
+    {
+        _id :'7a422aa71b54a676234d17f8',
+        title: 'Third Title',
+        author: 'Edsgar W. Dijkstra',
+        url:'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 15,
+        __v: 0
+    }
+    
+    ]
+
+
+
+    test('when list has only one blog, equals the like of that', () => {
+        assert.deepStrictEqual({likes: listHelper.favoriteBlog(blogs).likes},{likes : 15})
+    })
 
 })
--- part4/a-strucutre-backend-testing/blog-list-backend/utils/list_helper.js
+++ part4/a-strucutre-backend-testing/blog-list-backend/utils/list_helper.js
@@ -1,9 +1,9 @@
-const dummy = (blogs) => {
-    // ...
+const logger = require('../utils/logger')
 
+
+const dummy = () => {
     return 1
 }
-
 
 
 const totalLikes = (blogs) => {
@@ -16,8 +16,19 @@ const totalLikes = (blogs) => {
 
 }
 
+const favoriteBlog = (blogs) => {
+    
+    const maxLikedBlog = blogs.reduce((max, blog) => (
+        blog.likes > max.likes ? blog : max
+    ),blogs[0])
+
+    return maxLikedBlog
+
+}
+
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
