0cf55e559699018c7ed33c1aa3dad4e42f713af6
dhiegoferreira <dev.dhiegoferreira@gmail.com>
2025-03-13 17:08:57 +0000

    [p4-a] Ex.4.6

--- part4/a-strucutre-backend-testing/blog-list-backend/tests/list_helper.test.js
+++ part4/a-strucutre-backend-testing/blog-list-backend/tests/list_helper.test.js
@@ -31,16 +31,16 @@ describe('the most likes on blogs', () => {
     const blogs = [
     {
         _id :'5a422aa71b54a676234d17f8',
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsgar W. Dijkstra',
+        title: '1º Title',
+        author: 'Author1',
         url:'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
         likes: 5,
         __v: 0
     },
     {
         _id :'6a422aa71b54a676234d17f8',
-        title: 'Second TITLE',
-        author: 'Edsgar W. Dijkstra',
+        title: '2º Title',
+        author: 'Author1',
         url:'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
         likes: 10,
         __v: 0
@@ -48,19 +48,106 @@ describe('the most likes on blogs', () => {
     ,
     {
         _id :'7a422aa71b54a676234d17f8',
-        title: 'Third Title',
-        author: 'Edsgar W. Dijkstra',
+        title: '3º Title',
+        author: 'Author2',
         url:'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
         likes: 15,
         __v: 0
     }
-    
+    ,
+    {
+        _id :'7a422aa71b54a676234d17f8',
+        title: '4º Title',
+        author: 'Author2',
+        url:'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 15,
+        __v: 0
+    },
+    {
+        _id :'7a422aa71b54a676234d17f8',
+        title: '5º Title',
+        author: 'Author3',
+        url:'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 15,
+        __v: 0
+    }
     ]
 
 
 
     test('when list has only one blog, equals the like of that', () => {
         assert.deepStrictEqual({likes: listHelper.favoriteBlog(blogs).likes},{likes : 15})
+        
+    })
+
+})
+
+
+
+
+
+describe('largest amount of blogs', () => {
+
+
+    const blogs = [
+    {
+        _id :'5a422aa71b54a676234d17f8',
+        title: '1º Title',
+        author: 'Author1',
+        url:'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 5,
+        __v: 0
+    },
+    {
+        _id :'6a422aa71b54a676234d17f8',
+        title: '2º Title',
+        author: 'Author1',
+        url:'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 10,
+     
+        __v: 0
+    },
+    {
+        _id :'6a422aa71b54a676234d17f8',
+        title: '2º Title',
+        author: 'Author1',
+        url:'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 10,
+        __v: 0
+    }
+    ,
+    {
+        _id :'7a422aa71b54a676234d17f8',
+        title: '3º Title',
+        author: 'Author2',
+        url:'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 15,
+        __v: 0
+    }
+    ,
+    {
+        _id :'7a422aa71b54a676234d17f8',
+        title: '4º Title',
+        author: 'Author2',
+        url:'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 15,
+        __v: 0
+    },
+    {
+        _id :'7a422aa71b54a676234d17f8',
+        title: '5º Title',
+        author: 'Author3',
+        url:'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 15,
+        __v: 0
+    }
+    ]
+
+
+
+    test('should return the author with the most amount of blogs', () => {
+        assert.deepStrictEqual(listHelper.mostBlogs(blogs),{author: 'Author1', blogs: 3})
+        
     })
 
 })
--- part4/a-strucutre-backend-testing/blog-list-backend/utils/list_helper.js
+++ part4/a-strucutre-backend-testing/blog-list-backend/utils/list_helper.js
@@ -26,9 +26,65 @@ const favoriteBlog = (blogs) => {
 
 }
 
+const mostBlogs = (blogs) => {
+
+    //get list of authors
+    const authors = blogs.map((blog) => {
+        return blog.author
+    }) 
+
+    //distinct authors
+    const distictAuthors = [...(new Set(authors))]
+
+
+
+    const topAuthors = []
+    distictAuthors.forEach((author) => {
+        var blogsByAuthor = blogs.filter((blog) => {
+            return blog.author === author
+        })
+
+        topAuthors.push({
+            author: author,
+            blogs: blogsByAuthor.length
+        })
+    })
+
+    if(topAuthors.length === 0)
+        return 
+
+    const mostBlogs = topAuthors.reduce((max, tops) => (
+        tops.blogs > max.blogs ? tops : max
+    ), [topAuthors[0]])
+
+    return mostBlogs[0]
+
+    //1 - count the amount of blogs of each author or blog object in blogs and put on this structure
+    /*
+    {
+        author: "Robert C. Martin"
+        blogs: 3
+    }
+
+    */
+    // {
+    //     _id :'6a422aa71b54a676234d17f8',
+    //     title: 'Second TITLE',
+    //     author: 'Edsgar W. Dijkstra',
+    //     url:'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    //     likes: 10,
+    //     __v: 0
+    // }
+    // ,
+    
+
+
+}
+
 
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs
 }
